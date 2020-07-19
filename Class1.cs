4f67b194a3607cd932223f48b6da9d03cb3ffee2
unknown <bruna.yoshie@iventis.com.br>
2020-07-19 20:11:21 -0300

    first commit

---
+++ Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO__1
+{
+    class ContaBancaria
+
+    {
+        public string conta { get; set; }
+        public string agencia{get; set; }
+
+        internal void ImprimirSaldo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Nome { get; set; }
+        public string senha { private get; set; }
+        private int Saldo;
+
+        public ContaBancaria(int saldo)
+        {
+            Saldo = saldo;
+        }
+
+        public ContaBancaria()
+        {
+        }
+
+        public void Depositar(int valor)
+        {
+            Saldo = Saldo + valor;
+        }
+
+        public void Sacar(int valor)
+        {
+            Saldo = Saldo - valor;
+        }
+
+        public int ConsultarSaldo()
+        {
+            int taxa = 10;
+            int valortaxa = (Saldo * taxa) / 100;
+            return Saldo = valortaxa;
+        }
+
+        public void ImprimirSaldo (int saldo)
+        {
+            Console.WriteLine($"O saldo da sua conta {conta} na agencia {agencia} é: {ConsultarSaldo()}");
+        }     
+
+        
+
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace POO__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contabancaria = new ContaBancaria();
+
+            contabancaria.agencia = "123";
+            contabancaria.conta = "321";
+            contabancaria.Nome = "Bruna";
+            contabancaria.senha = "123";
+            contabancaria.ImprimirSaldo();
+           
+
+
+        }
+    }
+}
